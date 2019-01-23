30ee8b834eeecc4c4157c8f4b1795eab2d9231e9
ngan <ngan@localhost.localdomain>
2019-01-23 14:48:06 +0000

    -

---
+++ sum2_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestBrokenSum(t *testing.T) {
+	actual := Sum(4, 2)
+	expected := 3
+	if actual != expected {
+		t.Errorf("Sum(1, 2): actual %v, expected %v", actual, expected)
+	}
+}
