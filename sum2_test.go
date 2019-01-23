a36b5a369cf5d59c18c2a7184cf8f1927ecee445
ngan <ngan@localhost.localdomain>
2019-01-23 15:32:39 +0000

    Fixed

--- sum2_test.go
+++ sum2_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestBrokenSum(t *testing.T) {
 	actual := Sum(4, 2)
-	expected := 3
+	expected := 6
 	if actual != expected {
 		t.Errorf("Sum(1, 2): actual %v, expected %v", actual, expected)
 	}
